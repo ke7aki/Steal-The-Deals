8f34824aa6efd61a2402268dbf160a0bc4730cf3
Ketaki <gawdeketaki7@gmail.com>
2023-06-13 02:07:53 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# StealTheDeal
+An android app.
+This app provides details of deals which are available in firebase-database. Other than that fragments are used. Login, logout, regiter, change password, map activity, everyhing is working.
+
+This android app is made by me for my android project. Later I decided to put it on github for laerning purpose to others.
+
+I have used custom list vew, splash activity and firbase data connectivity.
+App is built for API 26(Marshmallow) and above.
+Gradle is used 4.0 and firebase modules versions are >=11.2.0
+
+App requires internet with >=120 kbps spped.
+
+NOTE: Update google.json with your firebase credentials for login and signup to be working.
---
+++ app/src/main/java/pentagon/stealthedeal/MainActivity.java
@@ -0,0 +1,138 @@
+package pentagon.stealthedeal;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+
+
+
+
+
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.StealTheDeal.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public Toolbar toolbar;
+    private BottomNavigationView bottomNav;
+    private FrameLayout mainFrame;
+    private HomeFragment homeFragment;
+    private FoodFragment foodFragment;
+    private FashionFragment fashionFragment;
+    private FunFragment funFragment;
+    private ProfileFragment profileFragment;
+    private Bundle bundle;
+
+    public final static String ACTIVE_USER = "User Session";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String activeUser = intent.getStringExtra(ACTIVE_USER);
+        bundle = new Bundle();
+        bundle.putString("userId",activeUser);
+
+        toolbar = (Toolbar) findViewById(R.id.maintoolbar);
+        toolbar.setTitle("Latest Deals");
+        bottomNav = (BottomNavigationView) findViewById(R.id.bottomnav);
+        mainFrame = (FrameLayout) findViewById(R.id.mainframe);
+        homeFragment = new HomeFragment();
+        foodFragment = new FoodFragment();
+        fashionFragment = new FashionFragment();
+        profileFragment = new ProfileFragment();
+        profileFragment.setArguments(bundle);
+        funFragment = new FunFragment();
+
+        setFragment(homeFragment);
+
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+
+                    case R.id.navhome:
+                        setFragment(homeFragment);
+                        toolbar.setTitle("Latest Deals");
+                        return true;
+
+                    case R.id.navfashion:
+                        setFragment(fashionFragment);
+                        toolbar.setTitle("Fashion Deals");
+                        return true;
+
+                    case R.id.navfood:
+                        setFragment(foodFragment);
+                        toolbar.setTitle("Food Deals");
+                        return true;
+
+                    case R.id.navfun:
+                        setFragment(funFragment);
+                        toolbar.setTitle("Entertainment Deals");
+                        return true;
+
+                    case R.id.navprofile:
+                        setFragment(profileFragment);
+                        toolbar.setTitle("Profile");
+                        return true;
+
+                        default:
+                            return false;
+                }
+            }
+        });
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.mainframe, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle(R.string.app_name);
+        builder.setIcon(R.mipmap.ic_launcher);
+        builder.setMessage("Do you want to exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        FirebaseAuth.getInstance().signOut();
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        FirebaseAuth.getInstance().signOut();
+        finish();
+    }
+}
